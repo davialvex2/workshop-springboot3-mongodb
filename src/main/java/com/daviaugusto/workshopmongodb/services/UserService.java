bcf6db6523e2ae86133a9ec82bdfe7dd65cd58a3
Davi Augusto <davi.miranda0919@gmail.com>
2025-01-15 17:47:12 -0300

    Método insert user

--- src/main/java/com/daviaugusto/workshopmongodb/services/UserService.java
+++ src/main/java/com/daviaugusto/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.daviaugusto.workshopmongodb.domain.User;
+import com.daviaugusto.workshopmongodb.dto.UserDTO;
 import com.daviaugusto.workshopmongodb.exception.ObjectNotFoundException;
 import com.daviaugusto.workshopmongodb.repositories.UserRepositoty;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert (User obj) {
+		 return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getNome(), obj.getEmail());
 	}
 
 }
