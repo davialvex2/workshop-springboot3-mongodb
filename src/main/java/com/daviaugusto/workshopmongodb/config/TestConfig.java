81fc6059cdea0d5e29155a9126a64cc627306494
Davi Augusto <davi.miranda0919@gmail.com>
2025-01-16 22:17:55 -0300

    Criando classe Post

--- src/main/java/com/daviaugusto/workshopmongodb/config/TestConfig.java
+++ src/main/java/com/daviaugusto/workshopmongodb/config/TestConfig.java
@@ -1,13 +1,16 @@
 package com.daviaugusto.workshopmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
-
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.daviaugusto.workshopmongodb.domain.Post;
 import com.daviaugusto.workshopmongodb.domain.User;
+import com.daviaugusto.workshopmongodb.repositories.PostRepositoty;
 import com.daviaugusto.workshopmongodb.repositories.UserRepositoty;
 
 
@@ -18,19 +21,29 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private UserRepositoty repository;
 	
+	@Autowired
+	private PostRepositoty postRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		repository.deleteAll();
+		postRepository.deleteAll();
 		
 		User davi = new User(null, "Davi Augusto", "davi@gmail.com");
 		User daniel = new User(null, "Daniel Miranda", "daniel@gmail.com");
 		User suzy = new User(null, "Suzy Cristina", "suzy@gmail.com");
 		User maria = new User(null, "Maria Santos", "maria@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("18/05/2023"), "Aniversário", "Parabens para mim, 18 anos", maria);
+		Post post2 = new Post(null, sdf.parse("15/03/2023"), "Receita de Bolo", "Coloque três chicaras de açucar, depois acrescente leite", suzy);
 		
 		repository.saveAll(Arrays.asList(daniel, suzy, maria, davi));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	
 	}
---
+++ src/main/java/com/daviaugusto/workshopmongodb/domain/Post.java
@@ -0,0 +1,90 @@
+package com.daviaugusto.workshopmongodb.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Id;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date data;
+	private String titulo;
+	private String corpo;
+	private User autor;
+	
+	public Post() {
+	}
+
+	public Post(String id, Date data, String titulo, String corpo, User autor) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.titulo = titulo;
+		this.corpo = corpo;
+		this.autor = autor;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getCorpo() {
+		return corpo;
+	}
+
+	public void setCorpo(String corpo) {
+		this.corpo = corpo;
+	}
+
+	public User getAutor() {
+		return autor;
+	}
+
+	public void setAutor(User autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Post))
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
